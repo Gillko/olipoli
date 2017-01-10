a7a3f851248a014474391a1404576fcf83560054
Gilko <gilles_vanpeteghem@hotmail.com>
2017-01-10 11:11:07 +0100

    5th Commit - AngularJS

---
+++ public/assets/js/angular.js
@@ -0,0 +1,26 @@
+angular.module("olipoli", [])
+
+.controller('mainCtrl', function($scope, $http){
+	
+	$http.get('http://localhost:8888/public/packages_json')
+		.then(function(res){
+			$scope.packages = res.data;
+		});
+	})
+.filter('unique', function () {
+	return function(collection, keyname) {
+		var output = [], 
+		keys = [];
+
+	  angular.forEach(collection, function(item) {
+
+		var key = item[keyname];
+		if(keys.indexOf(key) === -1) {
+			keys.push(key);
+
+			output.push(item);
+		}
+	});
+		return output;
+	};
+});
