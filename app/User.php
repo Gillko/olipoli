d7173b779707a1e9325d0e85ce3845109f77fd47
Gilko <gilles_vanpeteghem@hotmail.com>
2017-01-17 20:27:52 +0100

    16th Commit - changed the User model for Auth

--- app/User.php
+++ app/User.php
@@ -2,28 +2,42 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+	use Notifiable;
+	
+	/**
+	* The attributes that are mass assignable.
+	*
+	* @var array
+	*/
+	protected $fillable = ['user_firstname', 'user_surname', 'user_fullname', 'user_username', 'email', 'password'];
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+	protected $table ='users';
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+	protected $primaryKey = 'user_id';
+
+	public $timestamps = false;
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [
+		'password', 'remember_token',
+	];
+
+	/**
+	* A user has many roles
+	*
+	* @return \Illuminate\Database\Eloquent\Relations\HasMany
+	*/
+	public function roles()
+	{
+		return $this->hasMany('App\Role');
+	}
 }
