8dbb46b9fd8d1326b0d99c33f2476c7b8ef6e529
Gilko <gilles_vanpeteghem@hotmail.com>
2017-01-17 20:26:49 +0100

    15th Commit - commented some code in the edit view of a type

--- resources/views/types/edit.blade.php
+++ resources/views/types/edit.blade.php
@@ -6,12 +6,10 @@
 		{{ Form::text('type_name', null, ['class' => 'form-control']) }}
 		{{ Form::label('type_description', 'Description')}}
 		{{ Form::text('type_description', null, ['class' => 'form-control']) }}
-		{{ Form::label('items', 'Item')}}
-		{{ Form::select('item_list[]', $items, null, ['multiple', 'class' => 'form-control']) }}
+		{{-- {{ Form::label('items', 'Item')}}
+		{{ Form::select('items', $items, null, ['multiple', 'class' => 'form-control']) }} --}}
 		{{ Form::label('packages', 'Package')}}
 		{{ Form::select('package_list[]', $packages, null, ['multiple', 'class' => 'form-control']) }}
-		
-
 		{{ Form::submit('Edit the Type!', array('class' => '')) }}
 	{{ Form::close() }}
 @endsection
