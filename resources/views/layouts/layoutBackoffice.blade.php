893ae755ca7876221921b50d1803ac6c4f460e2e
Gilko <gilles_vanpeteghem@hotmail.com>
2017-01-30 17:43:12 +0100

    31st Commit - changes to the layout file

--- resources/views/layouts/layoutBackoffice.blade.php
+++ resources/views/layouts/layoutBackoffice.blade.php
@@ -5,6 +5,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="">
 		<meta name="keywords" content="">
+		<link rel="shortcut icon" href="{{ url('assets/img/favicon.ico') }}" type="image/x-icon">
+		<link rel="icon" href="{{ url('assets/img/favicon.ico') }}" type="image/x-icon">
 		<!-- META FACEBOOK SHARE-->
 		<meta property="og:title" content="" />
 		<meta property="og:type" content="website" />
@@ -27,17 +29,27 @@
 						<h1>Backoffice Olipoli</h1>
 						@if (Auth::guest())
 							<ul class="nav nav-pills nav-stacked">
+								<li><a href="{{ url('/') }}">Homepage</a></li>
+								<li><a href="{{ url('navigations') }}">Navigations</a></li>
+								<li><a href="{{ url('listitems') }}">Listitems</a></li>
 								<li><a href="{{ url('packages') }}">Packages</a></li>
 								<li><a href="{{ url('items') }}">Items</a></li>
 								<li><a href="{{ url('types') }}">Types</a></li>
+								<li><a href="{{ url('contents') }}">Contents</a></li>
+								<li><a href="{{ url('pictures') }}">Pictures</a></li>
 							</ul>
 						@else
 							<ul class="nav nav-pills nav-stacked">
 								<li>Welcome {{ Auth::user()->user_username }}</li>
 								<li><a href="{{ url('logout') }}">Logout</a></li>
+								<li><a href="{{ url('/') }}">Homepage</a></li>
+								<li><a href="{{ url('navigations') }}">Navigations</a></li>
+								<li><a href="{{ url('listitems') }}">Listitems</a></li>
 								<li><a href="{{ url('/packages') }}">Packages</a></li>
 								<li><a href="{{ url('items') }}">Items</a></li>
 								<li><a href="{{ url('types') }}">Types</a></li>
+								<li><a href="{{ url('contents') }}">Contents</a></li>
+								<li><a href="{{ url('pictures') }}">Pictures</a></li>
 							</ul>
 						@endif
 					</div>
