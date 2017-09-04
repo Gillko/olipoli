d509da7ca951bb3e6f656fbe29368de22060a23c
Gilko <gilles_vanpeteghem@hotmail.com>
2017-09-04 20:38:47 +0200

    82nd Commit - change to the package blade file

--- resources/views/includes/package.blade.php
+++ resources/views/includes/package.blade.php
@@ -2,7 +2,7 @@
 <span class="line">&nbsp;</span>
 <p class="package-description-op" ng-if="package.package_description != 0"><b>{[{package.package_description}]}</b></p>
 <div class="package-styles-op">
-	<div class="types-op" ng-repeat="type in package.types">
+	<div class="types-op" ng-repeat="type in package.types" ng-if="type.type_name != 0">
 		<h2 class="subheading-package"><b>{[{type.type_name}]}</b></h2>
 		<p class="item-op" ng-repeat="item in package.items" ng-if="type.type_id == item.type_id">{[{item.item_description}]}</p>
 	</div>
