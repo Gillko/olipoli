45d105b02d7b42feb8480a1f056c8f6d1942ef33
Gilko <gilles_vanpeteghem@hotmail.com>
2017-09-04 21:50:08 +0200

    85th Commit - updating all files to get last version of olipoli atm

--- _olipoli_new_grunt/src/assets/js/main.js
+++ _olipoli_new_grunt/src/assets/js/main.js
@@ -1,3 +1,18 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = Boolean(10 > 9);
-}
+$(document).ready(function() {
+	$(function() {
+		$(document).on('click', 'a.link', function(event){
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: $( $.attr(this, 'href')).offset().top
+			}, 500);
+		});
+	});
+	$(function() {
+		$(document).on('click', 'button.rsTmb', function(event){
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: $( $.attr(this, 'href')).offset().top
+			}, 500);
+		});
+	});
+});
