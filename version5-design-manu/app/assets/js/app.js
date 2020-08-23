3f433f334d146b826a8d3ac2e7d49f9e605642c8
Gilko <gilles_vanpeteghem@hotmail.com>
2020-08-23 22:51:05 +0200

    113th Commit - header and navigation tablet and mobile

--- version5-design-manu/app/assets/js/app.js
+++ version5-design-manu/app/assets/js/app.js
@@ -7,4 +7,15 @@ $(document).ready(function(){
 
 	var active__link = document.querySelector('a.' + current__page);
 	active__link.className = active__link.className + ' header__navigation-active';
+
+	var active__link_mobile = document.querySelector('li.' + current__page);
+	active__link_mobile.className = active__link_mobile.className + ' header__navigation-active-mobile';
+
+
+	//hamburger navigation
+	$('.header__navigation-hamburger').click(function(){
+		$(this).toggleClass('open');
+
+		$('.header__navigation').toggleClass('open');
+	});
 });
