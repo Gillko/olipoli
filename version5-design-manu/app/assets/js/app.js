49b8f434f70e38e30510de79688e47431ee92587
Gilles Vanpeteghem <gilles@adagio.company>
2020-08-28 15:22:28 +0200

    118th Commit - references page and load more button

--- version5-design-manu/app/assets/js/app.js
+++ version5-design-manu/app/assets/js/app.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 	//jquery
-    //var current__page = $('body').attr('class');
+	//var current__page = $('body').attr('class');
 
-    //javascript
+	//javascript
 	var current__page = document.getElementsByTagName('body')[0].className;
 
 	var active__link = document.querySelector('a.' + current__page);
@@ -17,5 +17,34 @@ $(document).ready(function(){
 		$(this).toggleClass('open');
 
 		$('.header__navigation').toggleClass('open');
+	});
+
+	//showing more before after references
+	var $button					= '#button';
+	var $hiddenCarFirst 		= '#before-after .before-after__car__hidden:first';
+	var $hiddenCar 				= '#before-after .before-after__car__hidden';
+	var $removeClassHiddenCar 	= 'before-after__car__hidden';
+	var $scrollToCar			= '#before-after__car__scroll-to';
+
+	var $car 					= '#before-after .before-after__car';
+
+	$($button).click(function(){
+		$($hiddenCar + ':hidden').slice(0,2).fadeIn().scrollTop();
+
+		$($car).removeAttr('id');
+
+		$($hiddenCarFirst).attr('id', 'before-after__car__scroll-to');
+		
+		$($hiddenCarFirst).removeClass($removeClassHiddenCar);
+
+		$($hiddenCar + ':eq(0)').removeClass($removeClassHiddenCar);
+
+		$('html, body').animate({
+			scrollTop: $($scrollToCar).offset().top
+		}, 1000);
+
+		if($($car).length == $($car + ':visible').length){
+			$($button).hide();
+		}
 	});
 });
